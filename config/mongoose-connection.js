811e32a42b50dcca702dea6b1b546071fbcd258b
Yashik Jain <yashikjain.160805@gmail.com.com>
2025-10-17 23:51:19 +0530

    commit on october 17

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
-
+const dbgr = require("debug")("development:mongoose");
+const config = require("config")
 mongoose
-.connect("mongodb://localhost:27017/scatch")
+.connect(`${config.get("MONGODB_URI")}`)
 .then(function(){
-console.log("Connected")
+dbgr("Connected")
 })
 .catch(function(err) {
-    console.log(err)
+    dbgr(err)
 })
 
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,5 +1,5 @@
 const express = require("express")
-
+const ownermodel = require("../models/owners.models.js")
 const router = express.Router();
 
 
@@ -7,4 +7,18 @@ router.get("/",function(req,res){
     res.send("Hey")
 })
 
+
+if(process.env.NODE_ENV==="development") {
+    router.post("/create",async function(req,res) {
+        let owners = await ownermodel.find();
+        if(owners.length>0) return res.send(503).send("Cannot create new owners..");
+        let {fullname,email,password} = req.body();
+       let createduser = await ownermodel.create({
+            fullname,
+            email,
+            password
+        })
+        res.status(202).send(createduser);
+    });
+}
 module.exports = router;
